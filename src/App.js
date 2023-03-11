8d0b7f0e090aa42691e4be6a443864154789a0f1
Zimmer Kennedy <kennedyzimmer00@gmail.com>
2023-03-10 15:17:57 -0900

    Project Start

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='text=3xl font-bold underline'>
+      Hello
+      </div>
+    </div>
+  );
+}
+
+export default App;
